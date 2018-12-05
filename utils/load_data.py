415b3d637c35dcd95ee7c029859125d437febce7
Homero Gabriel Roman Roman <homerroman@gmail.com>
2018-12-04 22:26:03 -0800

    mnist support expand_dims

--- utils/load_data.py
+++ utils/load_data.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 from datasets.imagenet import load_imagenet
 from datasets.cifar10 import load_cifar10
 from datasets.mnist import load_mnist
@@ -9,8 +11,7 @@ def load_data(args, data_partition="train"):
         X,Y = load_imagenet(args.batch_size, classes, shape, args.data_dir +"/"+data_partition)
     elif args.dataset=='cifar10':
         X,Y = load_cifar10(args.num_classes)
-        X = X[:int(X.shape[0]*0.3)]
-        Y = Y[:int(Y.shape[0]*0.3)]
     else:
         X,Y = load_mnist(args.num_classes)
+        X = np.expand_dims(X,axis=3)
     return X,Y
